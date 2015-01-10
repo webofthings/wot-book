e70a2936ea5129c209b099b6eacc2271810dce45
Dom Guinard <dev@guinard.org>
2015-01-10 16:27:25 +0000

    Adding new examples

--- proximity.js
+++ proximity.js
@@ -1,7 +1,7 @@
 var gpio = require("pi-gpio");
 
-gpio.open(7, "input", function(err) {     
-	    gpio.read(7, function(err, value) {
+gpio.open(12, "input", function(err) {     
+	    gpio.read(12, function(err, value) {
     	if(err) throw err;
     		console.log(value); // The current state of the pin
 		});
