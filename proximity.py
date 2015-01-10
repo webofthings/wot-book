59d424e134e44cc97b4e0885c8ad4cac40708d25
Dom Guinard <dev@guinard.org>
2015-01-10 16:51:30 +0000

    Python version of promimity sensing

--- proximity.py
+++ proximity.py
@@ -12,25 +12,23 @@ GPIO.setup(PinPIR, GPIO.IN)
 # Variables to hold the current and last states
 Current_State  = 0
 Previous_State = 0
-
 try:
   print "Waiting for PIR to settle ..."
   # Loop until PIR output is 0
-  while GPIO.input(PinPIR)==1:
+while GPIO.input(PinPIR) == 1:
   Current_State = 0
-
 while True:
   # Read PIR state
   Current_State = GPIO.input(PinPIR)
   # If the PIR is triggered
-  if Current_State==1 and Previous_State==0:
+  if Current_State == 1 and Previous_State == 0:
     print "  Motion detected!"
     # Record previous state
     Previous_State=1
 # If the PIR has returned to ready state
-elif Current_State==0 and Previous_State==1:
+elif Current_State == 0 and Previous_State == 1:
   print "  Ready"
-  Previous_State=0
+  Previous_State = 0
 # Wait for 10 milliseconds
 time.sleep(0.01)
 except KeyboardInterrupt:
