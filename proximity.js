447c5e35a611a988b285312c204faa8c2e64cc62
Dom Guinard <dev@guinard.org>
2015-01-11 18:15:17 +0000

    Node.js version

--- proximity.js
+++ proximity.js
@@ -1,10 +1,11 @@
 var gpio = require("pi-gpio");
 
-gpio.close(12);
-gpio.open(12, "input", function(err) {     
-	    gpio.read(12, function(err, value) {
+var pin = 11;
+
+gpio.open(pin, "input", function(err) {     
+	gpio.read(pin, function(err, value) {
     	if(err) throw err;
-    		console.log(value); // The current state of the pin
-		});
-		gpio.close(12);
+    	console.log(value); // The current state of the pin
+		gpio.close(pin);
+	});
 });
