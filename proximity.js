3c16d2282167e6b628536f8eb01f7db5662a138a
Dom Guinard <dev@guinard.org>
2015-01-11 18:21:23 +0000

    Node.js version

--- proximity.js
+++ proximity.js
@@ -2,10 +2,26 @@ var gpio = require("pi-gpio");
 
 var pin = 11;
 
+// Start reading from stdin so we don't exit.
+process.stdin.resume();
+
+process.on('SIGINT', function () {
+	gpio.close(pin);
+	console.log('Closing GPIO...  Press Control-D to exit.');
+});
+
 gpio.open(pin, "input", function(err) {     
 	gpio.read(pin, function(err, value) {
     	if(err) throw err;
-    	console.log(value); // The current state of the pin
-		gpio.close(pin);
+    	readProximity();
 	});
 });
+
+
+function readProximity() {
+	gpio.read(pin, function(err, value) {
+    	if(err) throw err;
+    	console.log(value); // The current state of the pin
+		readProximity();
+	});
+}
