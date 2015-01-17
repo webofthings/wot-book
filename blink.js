cc63996ea7865482499b63eb5d7efcb8ae418272
Dom Guinard <dev@guinard.org>
2015-01-17 15:29:28 +0000

    Adding blink

--- blink.js
+++ blink.js
@@ -4,7 +4,8 @@ var inPin = 7;
 var status = 1;
 
 function blink() {
-	status = (status + 1) % 2
+	status = (status + 1) % 2;
+	console.log(status);
     gpio.open(inPin, "output", function(err) {     
 	    gpio.write(inPin, status, function() {          
 	        gpio.close(inPin);
