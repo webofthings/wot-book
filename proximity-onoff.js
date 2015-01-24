bf7b6de632d9425a63fbb441a37bf6e4e087d28b
Dom Guinard <dev@guinard.org>
2015-01-24 15:29:37 +0000

    Adding error handling

--- proximity-onoff.js
+++ proximity-onoff.js
@@ -2,11 +2,12 @@ var Gpio = require('onoff').Gpio,
     sensor = new Gpio(17, 'in');
 
 sensor.watch(function(err, value) {
-    if (err) exit();
+    if (err) exit(err);
     console.log("Change detected:" + value);
 });
 
-function exit() {
+function exit(err) {
+	if(err) console.log('An error occured:' + err);
     sensor.unexport();
     console.log('Bye, bye!')
 	process.exit(); 
