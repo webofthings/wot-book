580229e0c8f438b4c2791a7891fd6ce34bbb0051
Dom Guinard <dev@guinard.org>
2015-01-31 15:49:11 +0000

    Before cleaning

--- chapter2-hello-wot/server/server.js
+++ chapter2-hello-wot/server/server.js
@@ -1,9 +1,9 @@
 var express = require('express'),
-	cors = require('cors'),
-	app = express();
-
-var cons = require('consolidate')
-
+cors = require('cors'),
+app = express(),
+cons = require('consolidate'),
+http = require('http'),
+WebSocketServer = require('ws').Server;
 
 
 // This is just to get the external IP address to access this device
@@ -11,12 +11,12 @@ var os = require('os');
 var interfaces = os.networkInterfaces();
 var addresses = [];
 for (var k in interfaces) {
-    for (var k2 in interfaces[k]) {
-        var address = interfaces[k][k2];
-        if (address.family === 'IPv4' && !address.internal) {
-            addresses.push(address.address);
-        }
-    }
+	for (var k2 in interfaces[k]) {
+		var address = interfaces[k][k2];
+		if (address.family === 'IPv4' && !address.internal) {
+			addresses.push(address.address);
+		}
+	}
 }
 
 
@@ -26,15 +26,15 @@ app.set('view engine', 'html');
 app.set('views', __dirname + '/views');
 
 // Activate CORS
-app.use(cors());
+//app.use(cors());
 
 
 // Handler for internal server errors
 function errorHandler(err, req, res, next) {
-    console.error(err.message);
-    console.error(err.stack);
-    res.status(500);
-    res.render('error_template', { error: err });
+	console.error(err.message);
+	console.error(err.stack);
+	res.status(500);
+	res.render('error_template', { error: err });
 }
 
 app.use(errorHandler);
@@ -42,7 +42,7 @@ app.use(errorHandler);
 
 /*
 	device will need to have support for API keys
-*/
+	*/
 
 
 // We need a master object to hold the list of all sensors, actuators, config of the device, etc.
@@ -63,11 +63,11 @@ var devices = {
 		'tags':['raspberry','pi','WoT'],
 		'ip' : addresses[0],
 		'elements':{
-	   		'sensors':{'url':'sensors','doc':'The list of sensors'},
-	   		'actuators':{'url':'actuators','doc':'The list of actuators'},
-	   		'debug':{'url':'doc','doc':'The debug channel'},
-	   		'docs':{'url':'doc','doc':'The documentation for this device'}
-	   	}
+			'sensors':{'url':'sensors','doc':'The list of sensors'},
+			'actuators':{'url':'actuators','doc':'The list of actuators'},
+			'debug':{'url':'doc','doc':'The debug channel'},
+			'docs':{'url':'doc','doc':'The documentation for this device'}
+		}
 	},
 	'camera':{
 		'id':'klklsjfdamlsadk',
@@ -78,11 +78,11 @@ var devices = {
 		'tags':['camera','WoT'],
 		'ip' : addresses[0],
 		'elements':{
-	   		'sensors':{'url':'sensors','doc':'The list of sensors'},
-	   		'actuators':{'url':'actuators','doc':'The list of actuators'},
-	   		'debug':{'url':'doc','doc':'The debug channel'},
-	   		'docs':{'url':'doc','doc':'The documentation for this device'}
-	   	}
+			'sensors':{'url':'sensors','doc':'The list of sensors'},
+			'actuators':{'url':'actuators','doc':'The list of actuators'},
+			'debug':{'url':'doc','doc':'The debug channel'},
+			'docs':{'url':'doc','doc':'The documentation for this device'}
+		}
 	}
 }
 
@@ -92,80 +92,79 @@ var sensors = {
 		'gpio':{
 			'name':'General Purpose IO',
 			'description':'The general-purpose input-output pins.'},
-		'temperature':{
-			'name':'Temperature Sensor',
-			'description':'A temperature sensor.','unit':'celsius','value':22},
-		'pir':{
-			'name':'Passive Infrared',
-			'description':'A passive infrared sensor. When true someone is present.','type':'boolean','value':false}
-	},
-	'camera':{
-		'ccd':{
-			'name':'Camera Sensor',
-			'description':'The CCD sensor on the camera.'}
-	}
-}
-
-;
-
-var actuators = {
-	'pi':{
-		'display':{			
-			'name':'LCD Display screen',
-			'description':'A simple display that can write commands.',
-			'properties':[
-				{
-					'name':'brightness',
-					'timestamp':'today',
-					'value':80,
-					'unit':'%',
-					'type':'integer',
-					'description':'Percentace of brightness of the display. Min is 0 which is black, max is 100 which is white.'
-				},{
-					'name':'text'
-				}
-			],
-			'commands':['write','clear','blink','color','brightness']
-		},
-		'leds':{
-			'name':'General Purpose IO',
-			'description':'LEDs on the device',
-			'properties':[
-				{
-					'name':'rgb-led0',
-					'timestamp':'today',
-					'value':[255,100,10],
-					'unit':'intensity',
-					'type':['integer','integer','integer'],
-					'description':'Color setting of LED #0 in RGB. Min 0, max 255. Green is [0,255,0].'
+			'temperature':{
+				'name':'Temperature Sensor',
+				'description':'A temperature sensor.','unit':'celsius','value':22},
+				'pir':{
+					'name':'Passive Infrared',
+					'description':'A passive infrared sensor. When true someone is present.','type':'boolean','value':true}
 				},
-				{
-					'name':'rgb-led1',
-					'timestamp':'today',
-					'value':[255,100,10],
-					'unit':'intensity',
-					'type':['integer','integer','integer'],
-					'description':'Color setting of LED #1 in RGB. Min 0, max 255.'
-				},
-				{
-					'name':'binary-led0',
-					'timestamp':'today',
-					'value':true,
-					'unit':'intensity',
-					'type':'boolean',
-					'description':'Setting of binary LED #0. True for led on, false for led off.'
+				'camera':{
+					'ccd':{
+						'name':'Camera Sensor',
+						'description':'The CCD sensor on the camera.'}
+					}
 				}
-			]
-		}
-	},
-	'camera':{
-		'debug':{
-			'name':'Debug port',
-			'description':'A basic debug port to be used for reset & other admin functionality. Ideally do not use.'
-		}
-	}
-}
 
+				;
+
+				var actuators = {
+					'pi':{
+						'display':{			
+							'name':'LCD Display screen',
+							'description':'A simple display that can write commands.',
+							'properties':[
+							{
+								'name':'brightness',
+								'timestamp':'today',
+								'value':80,
+								'unit':'%',
+								'type':'integer',
+								'description':'Percentace of brightness of the display. Min is 0 which is black, max is 100 which is white.'
+							},{
+								'name':'text'
+							}
+							],
+							'commands':['write','clear','blink','color','brightness']
+						},
+						'leds':{
+							'name':'General Purpose IO',
+							'description':'LEDs on the device',
+							'properties':[
+							{
+								'name':'rgb-led0',
+								'timestamp':'today',
+								'value':[255,100,10],
+								'unit':'intensity',
+								'type':['integer','integer','integer'],
+								'description':'Color setting of LED #0 in RGB. Min 0, max 255. Green is [0,255,0].'
+							},
+							{
+								'name':'rgb-led1',
+								'timestamp':'today',
+								'value':[255,100,10],
+								'unit':'intensity',
+								'type':['integer','integer','integer'],
+								'description':'Color setting of LED #1 in RGB. Min 0, max 255.'
+							},
+							{
+								'name':'binary-led0',
+								'timestamp':'today',
+								'value':true,
+								'unit':'intensity',
+								'type':'boolean',
+								'description':'Setting of binary LED #0. True for led on, false for led off.'
+							}
+							]
+						}
+					},
+					'camera':{
+						'debug':{
+							'name':'Debug port',
+							'description':'A basic debug port to be used for reset & other admin functionality. Ideally do not use.'
+						}
+					}
+				}
 
 
 
@@ -173,14 +172,14 @@ var actuators = {
 app.get('/', function (req, res) {
 
 	res.format({
-	    html: function(){
-  			res.render('index', {names: Object.keys(devices), devices: devices, myself: myself });
-	    },
+		html: function(){
+			res.render('index', {names: Object.keys(devices), devices: devices, myself: myself });
+		},
 
-	    json: function(){
+		json: function(){
 			res.json(devices);
-	    }
-  	});	
+		}
+	});	
 
 	console.log('Request (%s %s)', req.method, req.url);
 })
@@ -191,30 +190,30 @@ app.get('/', function (req, res) {
 app.get('/:id', function (req, res) { // Gets a specific device
 
 	res.format({
-	    html: function(){
-  			res.render('device', {deviceId: req.params.id, device: devices[req.params.id]});
-	    },
+		html: function(){
+			res.render('device', {deviceId: req.params.id, device: devices[req.params.id]});
+		},
 
-	    json: function(){
+		json: function(){
 			res.json(devices[req.params.id]);
-	    }
-  	});	
+		}
+	});	
 
   	//res.send('user' + req.params.id);
 
-	console.log('Request (%s %s)', req.method, req.url);
-})
+  	console.log('Request (%s %s)', req.method, req.url);
+  })
 
 
 // accept GET on list of sensors
 app.get('/:id/sensors', function (req, res) {
 
 	res.format({
-	    html: function(){
-  			res.render('sensors', {deviceId: req.params.id, names: Object.keys(sensors[req.params.id]), device: devices[req.params.id], sensors: sensors[req.params.id]});
-	    },
+		html: function(){
+			res.render('sensors', {deviceId: req.params.id, names: Object.keys(sensors[req.params.id]), device: devices[req.params.id], sensors: sensors[req.params.id]});
+		},
 
-	    json: function(){
+		json: function(){
 			res.json(sensors[req.params.id]);	    
 		}
 	});	
@@ -229,11 +228,11 @@ app.get('/:id/sensors', function (req, res) {
 app.get('/:id/sensors/:type', function (req, res) {
 	
 	res.format({
-	    html: function(){
-  			res.render('sensor', {deviceId: req.params.id,  device: devices[req.params.id], sensors: sensors[req.params.id][req.params.type]});
-	    },
+		html: function(){
+			res.render('sensor', {deviceId: req.params.id,  device: devices[req.params.id], sensors: sensors[req.params.id][req.params.type]});
+		},
 
-	    json: function(){
+		json: function(){
 			res.json(sensors[req.params.id][req.params.type]);	    
 		}
 	});	
@@ -248,31 +247,31 @@ app.get('/:id/sensors/:type/value', function (req, res) {
 	var value = Math.random();
 	var result = {'value': value};
   	//res.json(result); // This generates a random value for this sensor
-	
-	res.format({
-	    html: function(){
+
+  	res.format({
+  		html: function(){
   			res.render('sensor', {deviceId: req.params.id,  device: devices[req.params.id], sensors: sensors[req.params.id][req.params.type]});
-	    },
+  		},
 
-	    json: function(){
-			res.json(result);	    
-		}
-	});	
+  		json: function(){
+  			res.json(result);	    
+  		}
+  	});	
 
-	console.log('Request (%s %s)', req.method, req.url)
+  	console.log('Request (%s %s)', req.method, req.url)
 
-})
+  })
 
 
 // accept GET on actuators
 app.get('/:id/actuators', function (req, res) {
 
 	res.format({
-	    html: function(){
-  			res.render('actuators', {deviceId: req.params.id,  device: devices[req.params.id], actuators: actuators[req.params.id]});
-	    },
+		html: function(){
+			res.render('actuators', {deviceId: req.params.id,  device: devices[req.params.id], actuators: actuators[req.params.id]});
+		},
 
-	    json: function(){
+		json: function(){
 			res.json(actuators[req.params.id]);	    
 		}
 	});	
@@ -284,11 +283,11 @@ app.get('/:id/actuators', function (req, res) {
 app.get('/:id/actuators/:type', function (req, res) {
 
 	res.format({
-	    html: function(){
-  			res.render('actuator', {deviceId: req.params.id,  device: devices[req.params.id], actuators: actuators[req.params.id]});
-	    },
+		html: function(){
+			res.render('actuator', {deviceId: req.params.id,  device: devices[req.params.id], actuators: actuators[req.params.id]});
+		},
 
-	    json: function(){
+		json: function(){
 			res.json(actuators[req.params.id][req.params.type]);	    
 		}
 	});	
@@ -301,7 +300,7 @@ app.get('/:id/actuators/:type', function (req, res) {
 
 // accept GET on subscriptions - NOT used
 app.get('/:id/subscriptions', function (req, res) {
-  	res.json(actuators);
+	res.json(actuators);
 	console.log('Request (%s %s)', req.method, req.url)
 })
 
@@ -309,7 +308,7 @@ app.get('/:id/subscriptions', function (req, res) {
 // accept POST on LEDs
 app.put('/:id/actuators/:type/', function (req, res) {
 	var result = {'status' : 'on'};
-  	res.json(result);
+	res.json(result);
 	console.log('Changing status of the LED to %s', result)
 
 	console.log('Request (%s %s)', req.method, req.url)
@@ -319,7 +318,34 @@ app.put('/:id/actuators/:type/', function (req, res) {
 
 // We now start the server
 var server = app.listen(3000, function () {
-  var host = server.address().address
-  var port = server.address().port
-  console.log('Your WoT device is running at http://%s:%s', addresses[0], port)
+	var host = server.address().address
+	var port = server.address().port
+	console.log('Your WoT device is running at http://%s:%s', addresses[0], port)
 })
+
+
+// W3C Standard Websocket Server for temperature
+var wss = new WebSocketServer({server: server});
+wss.on('connection', function(ws) {
+	var url = ws.upgradeReq.url;
+	var selected = null;
+	console.log(url);
+	if(url.indexOf('temperature') !== -1) {
+		console.log('Temperature selected!');
+		selected = sensors['pi']['temperature'];
+	} else if (url.indexOf('pir') !== -1) {
+		console.log('Pir selected!');
+		selected = sensors['pi']['pir'];
+	}
+
+	// Send update every second
+	// TODO: replace with actual updates from sensor
+	var id = setInterval(function() {
+		ws.send(JSON.stringify(selected), function() {});
+	}, 5000);
+	console.log('Started client interval...');
+	ws.on('close', function() {
+		console.log('Stopping client interval!');
+		clearInterval(id);
+	});
+});
