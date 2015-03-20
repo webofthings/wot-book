6675cc369e42815bee8d48d10a3a43d7f2922df5
Dom Guinard <dev@guinard.org>
2015-03-19 20:03:15 -0400

    Correcting names

---
+++ chapter4-gpios/listing-4.9-proximity-onoff.js
@@ -0,0 +1,21 @@
+var Gpio = require('onoff').Gpio,
+    sensor = new Gpio(17, 'in');    //#A
+
+sensor.watch(function(err, value) { //#B
+    if (err) exit(err);
+    console.log(value ? 'there is some one!' : 'not anymore!');
+});
+
+function exit(err) {
+	if(err) console.log('An error occurred: ' + err);
+    sensor.unexport();
+    console.log('Bye, bye!')
+	process.exit(); 
+}
+process.on('SIGINT', exit);
+
+/*
+#A Initialise pin 17 in input mode
+#B Listen for state changes on pin 17, if a change is detected
+the anonymous callback function will be called with the new value
+ */
