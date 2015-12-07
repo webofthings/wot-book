728b9ae58fb9610a12e41ff64a3547c1a3f788eb
Dom Guinard <dev@guinard.org>
2015-12-07 18:02:35 +0000

    Ensuring JSON is served when JQUERY requests it

--- chapter7-implementation/part1-2-direct-gateway/middleware/converter.js
+++ chapter7-implementation/part1-2-direct-gateway/middleware/converter.js
@@ -3,9 +3,16 @@ var msgpack = require('msgpack5')(),
   json2html = require('node-json2html');
 
 function represent(req, res, next) {
-  console.info('Representation converter middleware called!')
+  console.info('Representation converter middleware called!');
+
   if (req.result) { //#B
-    if (req.accepts('html')) { //#C
+    if (req.accepts('json')) { //#C
+      console.info('JSON representation selected!');
+      res.send(req.result);
+      return;
+    }
+
+    if (req.accepts('html')) {
       console.info('HTML representation selected!');
       var transform = {'tag': 'div', 'html': '${name} : ${value}'};
       res.send(json2html.transform(req.result, transform)); //#D
@@ -18,9 +25,11 @@ function represent(req, res, next) {
       res.send(encode(req.result)); //#E
       return;
     }
-    console.info('JSON representation selected!');
+
+    console.info('Defaulting to JSON representation!');
     res.send(req.result); //#F
     return;
+
   }
   else {
     next(); //#G
@@ -30,7 +39,7 @@ module.exports = represent;
 
 //#A Require the two modules and instantiate a MessagePack encoder
 //#B We check if the previous middleware left a result for us in res.result
-//#C Read the request header and check if the client requested HTML
+//#C Read the request header and check if the client requested JSON and serve the results
 //#D If HTML was requested we use the json2html to transform the JSON into a very simple HTML
 //#E For MessagePack we encode the JSON result into MessagePack using the encoder and return the result to the client
 //#F For all other formats we default to JSON
