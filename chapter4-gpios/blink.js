a05283277495bf690834e761c3452eed28359f60
vladounet <vladounet@gmail.com>
2015-12-12 14:56:24 +0000

    Fixed minor typos

--- chapter4-gpios/blink.js
+++ chapter4-gpios/blink.js
@@ -4,9 +4,9 @@ var Gpio = onoff.Gpio,
   led = new Gpio(4, 'out'), //#B
   interval;
 
-interval = setInterval(function () { //#B
-  var value = (led.readSync() + 1) % 2; //#C
-  led.write(value, function() { //#D
+interval = setInterval(function () { //#C
+  var value = (led.readSync() + 1) % 2; //#D
+  led.write(value, function() { //#E
     console.log("Changed LED state to: " + value);
   });
 }, 2000);
@@ -22,8 +22,7 @@ process.on('SIGINT', function () { //#F
 // #A Import the onoff library
 // #B Initialize pin 4 to be an output pin
 // #C This interval will be called every 2 seconds
-// #C Synchronously read the value of pin 4 and transform 1 to 0 or 0 to 1
-// #D Asynchronously write the new value to pin 4
-// #E Once the status has been written we set a timer to recursively call blink
+// #D Synchronously read the value of pin 4 and transform 1 to 0 or 0 to 1
+// #E Asynchronously write the new value to pin 4
 // #F Listen to the event triggered on CTRL+C
 // #G Cleanly close the GPIO pin before exiting
