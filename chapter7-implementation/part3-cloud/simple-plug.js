c1578ed91abafa151841996ff783e36c209615ec
Dom Guinard <dev@guinard.org>
2016-01-13 09:14:43 +0000

    Changing timeout to interval in simple-plug.js

--- chapter7-implementation/part3-cloud/simple-plug.js
+++ chapter7-implementation/part3-cloud/simple-plug.js
@@ -3,7 +3,8 @@ var mqtt = require('mqtt');
 var config = require('./config.json'); // #A 
 var thngId=config.thngId; 
 var thngUrl='/thngs/'+thngId;
-var thngApiKey=config.thngApiKey; 
+var thngApiKey=config.thngApiKey;
+var interval;
 
 console.log('Using Thng #'+thngId+' with API Key: '+ thngApiKey);
 
@@ -14,8 +15,9 @@ var client = mqtt.connect("mqtts://mqtt.evrythng.com:8883", {// #B
 
 client.on('connect', function () { // #C
   client.subscribe(thngUrl+'/properties/'); //#D
-  updateProperty ('livenow',true);
-  setTimeout(updateProperties, 5000); //#F
+  updateProperty('livenow', true); //#E
+
+  interval = setInterval(updateProperties, 5000); //#F
 });
 
 client.on('message', function (topic, message) { // #G
@@ -32,8 +34,6 @@ function updateProperties () {
 
   var power = (voltage * current * (0.6+Math.random()/10)).toFixed(3); // #J
   updateProperty ('power',power);
-
-  setTimeout(updateProperties, 5000); // #F
 }
 
 function updateProperty (property,value) {
@@ -42,16 +42,17 @@ function updateProperty (property,value) {
 
 // Let's close this connection cleanly
 process.on('SIGINT', function() { // #K
-  updateProperty ('livenow',false);
+  clearInterval(interval);
+  updateProperty ('livenow', false);
   client.end();
   process.exit();
 });
 
 //#A Load configuration from file (thng id and thng API Key)
 //#B Connects to the MQTT server on EVRYTHNG
-//#C Callback called once when the MQTT connection suceeded
+//#C Callback called once when the MQTT connection succeeded
 //#D Subscribe to All Properties
-//#E Set the property 'live' to true
+//#E Set the property 'livenow' to true on EVRYTHNG
 //#F Call the function updateProperties() in 5 seconds
 //#G Called every time an MQTT message is received from the broker
 //#H 'Measures' voltage (fluctuates around ~220 volts)
