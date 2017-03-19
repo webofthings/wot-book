aa01809678f4bf7b78c9796d0c777600439b6b63
Dominique Guinard <dev@guinard.org>
2017-03-19 12:44:35 +0000

    Fixes #23, wrong return type selected when q is specified

--- chapter7-implementation/part1-2-direct-gateway/middleware/converter.js
+++ chapter7-implementation/part1-2-direct-gateway/middleware/converter.js
@@ -2,29 +2,26 @@ var msgpack = require('msgpack5')(),
   encode = msgpack.encode, //#A
   json2html = require('node-json2html');
 
-module.exports = function() { //#B
+module.exports = function () { //#B
   return function (req, res, next) {
     console.info('Representation converter middleware called!');
-
     if (req.result) { //#C
-      if (req.accepts('html')) { //#D
-        console.info('HTML representation selected!');
-        var transform = {'tag': 'div', 'html': '${name} : ${value}'};
-        res.send(json2html.transform(req.result, transform)); //#E
-        return;
+      switch (req.accepts(['json', 'html', 'application/x-msgpack'])) { //#D
+        case 'html':
+          console.info('HTML representation selected!');
+          var transform = {'tag': 'div', 'html': '${name} : ${value}'};
+          res.send(json2html.transform(req.result, transform)); //#E
+          return;
+        case 'application/x-msgpack':
+          console.info('MessagePack representation selected!');
+          res.type('application/x-msgpack');
+          res.send(encode(req.result)); //#F
+          return;
+        default: //#G
+          console.info('Defaulting to JSON representation!');
+          res.send(req.result);
+          return;
       }
-
-      if (req.accepts('application/x-msgpack')) {
-        console.info('MessagePack representation selected!');
-        res.type('application/x-msgpack');
-        res.send(encode(req.result)); //#F
-        return;
-      }
-
-      console.info('Defaulting to JSON representation!');
-      res.send(req.result); //#G
-      return;
-
     }
     else {
       next(); //#H
@@ -34,7 +31,7 @@ module.exports = function() { //#B
 //#A Require the two modules and instantiate a MessagePack encoder
 //#B In Express, a middleware is usually a function returning a function
 //#C Check if the previous middleware left a result for you in req.result
-//#D Read the request header and check if the client requested HTML
+//#D Get the best representation to serve from the Accept header
 //#E If HTML was requested, use json2html to transform the JSON into simple HTML
 //#F Encode the JSON result into MessagePack using the encoder and return the result to the client
 //#G For all other formats, default to JSON
